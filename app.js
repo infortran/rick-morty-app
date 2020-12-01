74fec0f4b9a3c21d62a5b0853660971c26da4b14
Freddy Perez <freddy.perez.trabajos@gmail.com>
2020-11-30 23:45:26 -0300

    Navegacion entre paginas, ajustes finales de diseño responsivo

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
 /**
  * Creado por Freddy Pérez - 30-11-20.
  */
+const cardContainer = document.getElementById('card-container')
+
+const templateCard = document.getElementById('template-card').content
+
+const btnPrev = document.getElementsByClassName('prev')
+
+const btnNext = document.getElementsByClassName('next')
 
 
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    fetchPersonajes()
+const fragment = document.createDocumentFragment();
+
+const url = 'https://rickandmortyapi.com/api/character/?page=1'
+let next = '';
+let prev = '';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchPersonajes(url)
+
+    Array.from(btnNext).forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            fetchPersonajes(next)
+            window.scrollTo({top: 300, behavior: 'smooth'})
+        })
+    })
+
+    Array.from(btnPrev).forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            fetchPersonajes(prev)
+            window.scrollTo({top: 300, behavior: 'smooth'})
+        })
+    })
+
+
 })
 
-const fetchPersonajes = async () => {
+const fetchPersonajes = async (page) => {
     try{
-        const respuesta = await fetch("https://rickandmortyapi.com/api/character/")
+        const respuesta = await fetch(page)
         const data = await respuesta.json()
+        next = data.info.next
+        prev = data.info.prev
+        if(prev) {
+            Array.from(btnPrev).forEach((btn) => {
+                btn.classList.remove('d-none')
+            })
+        }else{
+            Array.from(btnPrev).forEach((btn) => {
+                btn.classList.add('d-none')
+            })
+        }
         renderPersonajes(data)
 
     }catch(error){
@@ -19,11 +60,30 @@ const fetchPersonajes = async () => {
     }
 }
 
-const renderPersonajes = (data) => {
-    data.results.forEach(producto => {
-        console.log(producto)
-    })
+const fetchPrevPersonajes = async () => {
+
 }
+
+const renderPersonajes = (personajes) => {
+    personajes.results.forEach(personaje => {
+        console.log(personaje.name)
+
+        templateCard.querySelector(".title").textContent = personaje.name
+        templateCard.querySelector('.img').src = personaje.image
+        templateCard.querySelector('#especie').textContent = personaje.species
+        templateCard.querySelector('#genero').textContent = personaje.gender
+        templateCard.querySelector('#estado').textContent = personaje.status
+        templateCard.querySelector('#localizacion').textContent = personaje.location.name
+
+        const clone = templateCard.cloneNode(true)
+
+        fragment.appendChild(clone)
+    })
+    cardContainer.innerHTML = '';
+    cardContainer.appendChild(fragment)
+}
+
+
 
 const filterPersonajes = (filter) => {
 
